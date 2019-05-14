3a030d9533ebae227a5312052deae7857cc00b08
John Phan <ktr2@cdc.gov>
2019-05-14 12:17:18 -0400

    update container tag

--- docker/build.sh
+++ docker/build.sh
@@ -2,5 +2,5 @@
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 cd ${SCRIPT_DIR}/..
-docker build -f ./docker/Dockerfile -t geneflow:$(cat ${SCRIPT_DIR}/../VERSION)-sing-3.1.1 .
+docker build -f ./docker/Dockerfile -t geneflow:$(cat ${SCRIPT_DIR}/../VERSION)--sing3.1.1 .
 
