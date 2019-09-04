4ffaece32f168f61fe057ffc343774bfecdfbceb
John Phan <ktr2@cdc.gov>
2019-09-04 11:32:51 -0400

    update version and changelog for python 3.7 compat

--- src/geneflow/agave_wrapper.py
+++ src/geneflow/agave_wrapper.py
@@ -6,7 +6,7 @@ import urllib.parse
 
 try:
     from agavepy.agave import Agave
-    from agavepy.asynch import AgaveAsyncResponse
+    from agavepy.asynchronous import AgaveAsyncResponse
 except ImportError: pass
 
 from geneflow.log import Log
