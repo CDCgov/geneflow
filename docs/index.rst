719651938b9b3d0cdcf897847bd0202450bd0076
John Phan <ktr2@cdc.gov>
2019-11-15 11:22:25 -0500

    add version option to cli

--- docs/index.rst
+++ docs/index.rst
@@ -3,7 +3,7 @@
 GeneFlow Documentation
 ======================
 
-Version: 1.11.0
+Version: 1.12.0
 
 GeneFlow (GF) is a light-weight platform-agnostic workflow engine for scientific computing.
 
--- src/geneflow/__init__.py
+++ src/geneflow/__init__.py
@@ -10,3 +10,7 @@ from pathlib import Path
 
 # GeneFlow module path
 GF_PACKAGE_PATH = Path.resolve(Path(__file__)).parents[0]
+
+# version info
+__version_info__ = ('1', '12', '0')
+__version__ = '.'.join(__version_info__)
